d7cb81f193a588718c2c9335ffa810d1ed188bcc
Vadim Radermacher <vadim.radermacher@gmail.com>
2019-02-04 16:21:24 +0100

    css changes

--- script.js
+++ script.js
@@ -16,54 +16,23 @@ window.localStorage.clear();
 
 let namesArray = [];
 let plansArray = [];
-let list = [];
 
 function addItem(name, plan){
 	let li = document.createElement("li");
-	let h2 = document.createElement("h2");
+	let h1 = document.createElement("h1");
 	let p = document.createElement("p");
-	let h2Content = document.createTextNode(name);
+	let h1Content = document.createTextNode(name);
 	let pContent = document.createTextNode(plan);
-	h2.appendChild(h2Content);
+	h1.appendChild(h1Content);
 	p.appendChild(pContent);
-	li.appendChild(h2);
+	li.appendChild(h1);
 	li.appendChild(p);
-	list.push(li);
 	document.querySelector(".list").appendChild(li);
-	let commentsArray = [];
-	let showdown  = require('showdown');
-  let converter = new showdown.Converter();
-	for (let i = 0; i < list.length; i++){
-	    (function(index){
-	        list[i].onclick = function(){
-	            document.querySelector(".modal-content").innerText = "";
-	            let div = document.createElement("div");
-	            let text = document.createTextNode(list[i].innerHTML);
-	            let comment = document.createElement("BUTTON");
-	            let textArea = document.createElement("TEXTAREA");
-	            textArea.class = "comment";
-	            let commentText = document.createTextNode("Comment");
-	            comment.appendChild(commentText);
-	            div.appendChild(text);
-				document.querySelector(".modal-content").innerHTML = div.innerText;
-				document.querySelector(".modal-content").appendChild(textArea);
-	            document.querySelector(".modal-content").appendChild(comment);
-	  			document.querySelector(".modal").style.display = "block";
-	  			comment.addEventListener("click", () => {
-	  				commentsArray.push(textArea.value);
-	  				window.localStorage.setItem( 'Comments' + i, JSON.stringify(commentsArray));
-	  			});
-
-    		}
-    	})(i);
-    }
 }
-
-// Ouverture de la fenêtre modale de création d'idée
 
 document.querySelector(".add").addEventListener("click", () => {
 	document.querySelector(".modal-content").innerText = "";
-	document.querySelector(".modal-content").innerHTML = '<h2>Creation of a new plan</h2><p>Your idea:</p><input type="text" name="test" class="name" placeholder="Enter idea"></input><p>Description of your plan to take over the world:</p><textarea type="text" name="plan" class="plan" placeholder="Enter Plan"></textarea><button class="submit">Submit</button>';
+	document.querySelector(".modal-content").innerHTML = '<h2>Creation of a new plan</h2><p>Your name:</p><input type="text" name="test" class="name" placeholder="Enter Name"></input><p>Your plan to take over the world:</p><textarea type="text" name="plan" class="plan" placeholder="Enter Plan"></textarea><button class="submit">Submit</button>';
 	document.querySelector(".modal").style.display = "block";
 
 	document.querySelector(".submit").addEventListener("click", () => {
@@ -75,32 +44,3 @@ document.querySelector(".add").addEventListener("click", () => {
 		document.querySelector(".modal").style.display = "none";
 	});
 });
-
-// Fermeture de la fenêtre modale lorqu'on clique en dehors
-
-window.onclick = function(event){
- 		if (event.target == document.querySelector(".modal")){
-   			document.querySelector(".modal").style.display = "none";
-  		}
-	}
-
-
-	/*TO DO: ajouter ces boutons à chaque li
-	<div id ="buttons">
-		<button id="edit"><i class="fas fa-pencil-alt"></i>Edit</button>
-		<button id="delete"><i class="fas fa-trash-alt"></i>Delete</button>
-</div>
-
-
-
-for (let i = 0; i < list.length; i++){
-    (function(index){
-        list[i].onclick = function(){
-            document.querySelector(".modal-content").innerText = "";
-			document.querySelector(".modal-content").innerHTML = list[i];
-  			document.querySelector(".modal").style.display = "block";
-  			console.log(list);
-    	}
-	})(i);
-}
-*/
